dddf8feb1656e273a5dc210aaefd5ada454e9fe3
abaduna~ <artuelrey@gmail.com>
2024-05-06 14:13:36 -0300

    feat: mesage of the error

--- blog/src/app/login/registrarse/page.tsx
+++ blog/src/app/login/registrarse/page.tsx
@@ -1,68 +1,94 @@
-"use client"
+"use client";
 
-import styles from "./styles.module.css"
-import { revalidatePath } from "next/cache"
-import { FormEvent, useState } from "react"
-import actionPath from "../../../action/cache"
-import { useRouter } from "next/navigation"
-export interface UserProps  {
-  user:string
-    password:string
+import styles from "./styles.module.css";
+import { revalidatePath } from "next/cache";
+import { FormEvent, useState } from "react";
+import actionPath from "../../../action/cache";
+import { useRouter } from "next/navigation";
+import { useFormik } from "formik";
+import { loginSchema } from "./scheme";
+export interface UserProps {
+  user: string;
+  password: string;
 }
-async function loginPost(user:UserProps) {
-  actionPath
-  const rest = await fetch("http://localhost:3001/api/login/created", {
-    method: "post",
-    headers: {
-      Accept: "application/json",
-      "Content-Type": "application/json",
-    },
 
-    body: JSON.stringify(user),
-  });
-
-  const data = await rest.json();
-  return data;
-}
 function Page() {
-  const [user,setUser]= useState<string>("")
-  const [password,setPassword]= useState<string>("")
-  const router = useRouter()
-    const handlerLogin =async (e: FormEvent<HTMLFormElement>)=>{
-        e.preventDefault()
-        const dataProps:UserProps = {
-          user,
-          password
-        }
-        try {
-         const token = await loginPost(dataProps)
+  const [user, setUser] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
+  const handlerLogin = async (values: { user: string; password: string }) => {
+    const dataProps: UserProps = {
+      user,
+      password,
+    };
+    try {
+      const token = await loginPost(dataProps);
 
-         
-         if (token) {
-
-          
-          localStorage.setItem("token", token.token);
-          router.push(`/blogs/${user}`)
-        } else {
-          console.log('El token es indefinido.');
-        }
-         
-        } catch (error) {
-          console.log(error);
-          
-        }
-        
+      if (token) {
+        localStorage.setItem("token", token.token);
+        router.push(`/blogs/${user}`);
+      } else {
+        console.log("El token es indefinido.");
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
+  async function loginPost(user: UserProps) {
+    actionPath;
+    const rest = await fetch("http://localhost:3001/api/login/created", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(user),
+    });
+
+    const data = await rest.json();
+    return data;
+  }
+  const formik = useFormik({
+    initialValues: {
+      user: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: (values) => {
+      handlerLogin(values);
+    },
+  });
   return (
     <div className={styles.formContainer}>
       <h1>Registrate</h1>
-        <form onSubmit={handlerLogin}>
-            <input placeholder="usuario" onChange={e=>setUser(e.target.value)}/>
-            <input placeholder="contraseña" type="password" onChange={e=>setPassword(e.target.value)}/>
-            <button>Ingresar</button>
-        </form>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          placeholder="usuario"
+          id="user"
+          name="user"
+          type="user"
+          onChange={formik.handleChange}
+          value={formik.values.user}
+        />
+         {formik.touched.user && formik.errors.user && (
+          <span className={styles.spanFrom}>{formik.errors.user}</span>
+        )}
+        <input
+          placeholder="contraseña"
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+         {formik.touched.password && formik.errors.password && (
+          <span className={styles.spanFrom}>{formik.errors.password}</span>
+        )}
+        <button>Ingresar</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
--- blog/src/app/login/signIn/page.tsx
+++ blog/src/app/login/signIn/page.tsx
@@ -1,18 +1,20 @@
-"use client"
+"use client";
 
-import styles from "./styles.module.css"
-import { revalidatePath } from "next/cache"
-import { useRouter } from 'next/navigation'
+import styles from "./styles.module.css";
+import { revalidatePath } from "next/cache";
+import { useRouter } from "next/navigation";
 
-import { FormEvent, useState } from "react"
-import action from "@/./action/cache"
+import { FormEvent, useState } from "react";
+import action from "@/./action/cache";
+import { useFormik } from "formik";
+import { loginSchema } from "./schema";
 
-export interface UserProps  {
-  user:string
-    password:string
+export interface UserProps {
+  user: string;
+  password: string;
 }
-async function loginPost(user:UserProps) {
-  action
+async function loginPost(user: UserProps) {
+  action;
   const rest = await fetch("http://localhost:3001/api/login/", {
     method: "post",
     headers: {
@@ -27,41 +29,65 @@ async function loginPost(user:UserProps) {
   return data;
 }
 function Page() {
-  const [user,setUser]= useState<string>("")
-  const [password,setPassword]= useState<string>("")
-  const router = useRouter()
-    const handlerLogin =async (e: FormEvent<HTMLFormElement>)=>{
-        e.preventDefault()
-        const data:UserProps = {
-          user,
-          password
-        }
-        try {
-         const token = await loginPost(data)
-         
-         console.log(token);
-         
-      
-          localStorage.setItem("token", token.token);
-          router.push(`/blogs/${user}`)
-       
-         
-        } catch (error) {
-          console.log(error);
-          
-        }
-        
+  const [user, setUser] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
+  const handlerLogin = async (values: { user: string; password: string }) => {
+    const data: UserProps = {
+      user: values.user,
+      password: values.password,
+    };
+    try {
+      const token = await loginPost(data);
+
+      console.log(token);
+
+      localStorage.setItem("token", token.token);
+      router.push(`/blogs/${values.user}`);
+    } catch (error) {
+      console.log(error);
     }
+  };
+  const formik = useFormik({
+    initialValues: {
+      user: "",
+      password: "",
+    },
+    validationSchema: loginSchema,
+    onSubmit: (values) => {
+      handlerLogin(values);
+    },
+  });
   return (
     <div className={styles.formContainer}>
       <h1>Iniciar secion</h1>
-        <form onSubmit={handlerLogin}>
-            <input placeholder="usuario" onChange={e=>setUser(e.target.value)}/>
-            <input placeholder="contraseña" type="password" onChange={e=>setPassword(e.target.value)}/>
-            <button>Ingresar</button>
-        </form>
+      <form onSubmit={formik.handleSubmit}>
+        <input
+          placeholder="usuario"
+          id="user"
+          name="user"
+          type="user"
+          onChange={formik.handleChange}
+          value={formik.values.user}
+        />
+         {formik.touched.user && formik.errors.user && (
+          <span className={styles.spanFrom}>{formik.errors.user}</span>
+        )}
+        <input
+          placeholder="contraseña"
+          id="password"
+          name="password"
+          type="password"
+          onChange={formik.handleChange}
+          value={formik.values.password}
+        />
+         {formik.touched.password && formik.errors.password && (
+          <span className={styles.spanFrom}>{formik.errors.password}</span>
+        )}
+        <button>Ingresar</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
